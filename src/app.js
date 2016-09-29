6308933fbd847f59496281dbcbad3008f95c4543
Chanlito <chanlito@Chanlitos-MacBook-Pro.local>
2016-09-29 13:25:41 +0700

    setup sockets

--- src/app.js
+++ src/app.js
@@ -1,22 +1,20 @@
 import bodyParser from 'koa-bodyparser'
 import error from 'koa-json-error'
-import Io from 'koa-socket'
 import Koa from 'koa'
 
 import config from './config'
+import socket from './sockets/index'
+import socketChat from './sockets/chat'
 
 const app = new Koa()
-const io = new Io()
 const port = config.port
 
 app.use(error())
 app.use(bodyParser())
-app.use(async(ctx, next) => {
-  ctx.io = io
-  await next()
-})
 
-io.attach(app)
+socket(app)
+socketChat(app)
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
--- src/sockets/chat.js
+++ src/sockets/chat.js
@@ -1,14 +1,16 @@
 import Io from 'koa-socket'
 export default (app) => {
-  const io = new Io()
+  const io = new Io({
+    namespace: 'chat'
+  })
   io.on('connection', ctx => {
-    console.log('Joined index', ctx.socket.id)
+    console.log('Joined chat', ctx.socket.id)
     io.broadcast('connections', {
       numConnections: io.connections.size
     })
   })
   io.on('disconnect', ctx => {
-    console.log('Left index', ctx.socket.id)
+    console.log('Left chat', ctx.socket.id)
     io.broadcast('connections', {
       numConnections: io.connections.size
     })
