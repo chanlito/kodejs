1e34d294b9597afbd95035aa513a895753961948
Bruce <bruce.hem@pathmazing.com>
2017-01-29 10:49:49 +0700

    es6xfy errorhandler

--- src/bootstrap/errorHandler.js
+++ src/bootstrap/errorHandler.js
@@ -1,19 +1,17 @@
-export default () => {
-  return async function (ctx, next) {
-    try {
-      await next()
-    } catch (err) {
-      console.log(err)
-      const code = err.status || err.statusCode || 500
-      const message = err.message || 'Internal Server Error'
-      ctx.set('Message', message)
-      ctx.status = code
-      ctx.body = {
-        code,
-        message,
-        ...err,
-        stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
-      }
+export default () => async (ctx, next) => {
+  try {
+    await next()
+  } catch (error) {
+    console.log(error)
+    const { message = 'Internal Server Error', stack, status, statusCode } = error
+    const code = status || statusCode || 500
+    ctx.set('Message', message)
+    ctx.status = code
+    ctx.body = {
+      code,
+      message,
+      ...error,
+      stack: process.env.NODE_ENV === 'development' ? stack : undefined
     }
   }
 }
